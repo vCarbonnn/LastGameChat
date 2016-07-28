cba7f9c11f3dda208d21824af6850ec2a553dd74
vCarbonnn <carbonontagpro@gmail.com>
2016-07-28 12:39:59 +0100

    Add @updateURL

--- ModToolsLastGameChat.user.js
+++ ModToolsLastGameChat.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         Mod Tools Last Game Chat
-// @author		 Carbon
+// @author	 	 Carbon
 // @version      0.3
+// @description  Small and helpful enhancement to Chat in TagPro Mod Tools
+// @updateURL    https://github.com/vCarbonnn/LastGameChat/raw/master/ModToolsLastGameChat.user.js
 // @include      http://tagpro-*.koalabeast.com/moderate/*
 // ==/UserScript==
 
