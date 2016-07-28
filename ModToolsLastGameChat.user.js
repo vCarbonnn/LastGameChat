2858ae8deaae1aff5a4b3ad111ded938d8ffb9c3
vCarbonnn <carbonontagpro@gmail.com>
2016-07-28 12:41:54 +0100

    Reindent lines because no one likes dodgy lines

--- ModToolsLastGameChat.user.js
+++ ModToolsLastGameChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mod Tools Last Game Chat
 // @author	 	 Carbon
-// @version      0.4
+// @version      0.5
 // @description  Small and helpful enhancement to Chat in TagPro Mod Tools
 // @updateURL    https://github.com/vCarbonnn/LastGameChat/raw/master/ModToolsLastGameChat.user.js
 // @include      http://tagpro-*.koalabeast.com/moderate/*
@@ -14,53 +14,53 @@ var matchVar = 0;
 if ((window.location.pathname.indexOf("chat") > -1)||(window.location.pathname.indexOf("userid") > -1)) {
     $(".buttons").append("<button id='lastGameChat' class='small' onclick='lastGameChat()'>Last Game Chat</button>");
     document.getElementById("lastGameChat").addEventListener('click',function () {
-         var filterIP = document.getElementById("filterIP").value;
-         var filterUserId = document.getElementById("filterUserId").value;
-         if (filterIP.match(/\S/)) {
-			 matchVar = 1;
-			 restoreIP = filterIP;
-             document.getElementById("filterIP").value = "";
-             document.getElementById("filterUserId").value = "";
-             document.getElementById("filterDisplayName").value = "";
-             document.getElementById("filterGameId").value = $('a[class ="dynamicFilter ipchecked"]')[0].innerText;
-             document.getElementById("filterButton").click();
-         }
-		 else if (filterUserId.match(/\S/)) {
-			 matchVar = 2;
-			 restoreID = filterUserId;
-             document.getElementById("filterIP").value = "";
-             document.getElementById("filterUserId").value = "";
-             document.getElementById("filterDisplayName").value = "";
-             document.getElementById("filterGameId").value = $('a[class ="dynamicFilter ipchecked"]')[0].innerText;
-             document.getElementById("filterButton").click();
-         }
-		$(".buttons").append("<button id='restoreChat' class='small' onclick='restoreChat()'>Restore Chat</button>");
-		document.getElementById("restoreChat").addEventListener('click',function () {
-			 document.getElementById("filterDisplayName").value = "";
-             document.getElementById("filterGameId").value = "";
-			 if(matchVar==1) {
-				 document.getElementById("filterIP").value = restoreIP;
-				 document.getElementById("filterUserId").value = "";
-			 }
-			 else if(matchVar==2) {
-				 document.getElementById("filterUserId").value = restoreID;
-				 document.getElementById("filterIP").value = "";
-			 }
-			document.getElementById("restoreChat").outerHTML='';
-             document.getElementById("filterButton").click();
-		});
-     });
+       var filterIP = document.getElementById("filterIP").value;
+       var filterUserId = document.getElementById("filterUserId").value;
+       if (filterIP.match(/\S/)) {
+        matchVar = 1;
+        restoreIP = filterIP;
+        document.getElementById("filterIP").value = "";
+        document.getElementById("filterUserId").value = "";
+        document.getElementById("filterDisplayName").value = "";
+        document.getElementById("filterGameId").value = $('a[class ="dynamicFilter ipchecked"]')[0].innerText;
+        document.getElementById("filterButton").click();
+    }
+    else if (filterUserId.match(/\S/)) {
+        matchVar = 2;
+        restoreID = filterUserId;
+        document.getElementById("filterIP").value = "";
+        document.getElementById("filterUserId").value = "";
+        document.getElementById("filterDisplayName").value = "";
+        document.getElementById("filterGameId").value = $('a[class ="dynamicFilter ipchecked"]')[0].innerText;
+        document.getElementById("filterButton").click();
+    }
+    $(".buttons").append("<button id='restoreChat' class='small' onclick='restoreChat()'>Restore Chat</button>");
+    document.getElementById("restoreChat").addEventListener('click',function () {
+        document.getElementById("filterDisplayName").value = "";
+        document.getElementById("filterGameId").value = "";
+        if(matchVar==1) {
+           document.getElementById("filterIP").value = restoreIP;
+           document.getElementById("filterUserId").value = "";
+       }
+       else if(matchVar==2) {
+           document.getElementById("filterUserId").value = restoreID;
+           document.getElementById("filterIP").value = "";
+       }
+       document.getElementById("restoreChat").outerHTML='';
+       document.getElementById("filterButton").click();
+   });
+});
 }
 
 if (window.location.pathname.indexOf("users") > -1) {
-        document.getElementById("filterName").onkeypress = function(e) {
-            var event = e || window.event;
-            var charCode = event.which || event.keyCode;
+    document.getElementById("filterName").onkeypress = function(e) {
+        var event = e || window.event;
+        var charCode = event.which || event.keyCode;
 
-            if ( charCode == '13' ) {
-                document.getElementById("filterHours").value = "whenever";
-                document.getElementById("filterButton").click();
-                return false;
-            }
-        };
+        if ( charCode == '13' ) {
+            document.getElementById("filterHours").value = "whenever";
+            document.getElementById("filterButton").click();
+            return false;
+        }
+    };
 }
