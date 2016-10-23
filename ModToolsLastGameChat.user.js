8f58a545a5467f9c434b8412be9363ec8b452c97
vCarbonnn <carbonontagpro@gmail.com>
2016-10-24 00:29:14 +0100

    Update ModToolsLastGameChat.user.js
    - Removed print line.
    - Improved line formatting.

--- ModToolsLastGameChat.user.js
+++ ModToolsLastGameChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mod Tools Last Game Chat
 // @author	 	 Carbon
-// @version      2.0
+// @version      2.1
 // @description  Small and helpful enhancement to Chat in TagPro Mod Tools
 // @updateURL    https://github.com/vCarbonnn/LastGameChat/raw/master/ModToolsLastGameChat.user.js
 // @include      http://tagpro-*.koalabeast.com/moderate/chat*
@@ -12,12 +12,11 @@ var filterIP;
 var filterUserId;
 
 $(".buttons").append("<button id='lastGameChat' class='small'>Last Game Chat</button>");
-console.log("added button");
 document.getElementById("lastGameChat").addEventListener('click',function () {
 	active = !active;
 	if(active) {
 		filterIP = document.getElementById("filterIP").value;
-        filterUserId = document.getElementById("filterUserId").value;
+    	filterUserId = document.getElementById("filterUserId").value;
     	getLastGameChat();
 	} else {
 		getRestoredChat();
@@ -26,14 +25,14 @@ document.getElementById("lastGameChat").addEventListener('click',function () {
 
 function getLastGameChat() {
 	try {
-		document.getElementById("filterGameId").value = document.getElementById("reportRows").children[0].children[0].innerText;
-		document.getElementById("filterIP").value = "";
+	    document.getElementById("filterGameId").value = document.getElementById("reportRows").children[0].children[0].innerText;
+	    document.getElementById("filterIP").value = "";
 	    document.getElementById("filterUserId").value = "";
 	    document.getElementById("filterDisplayName").value = "";
 	    document.getElementById("filterButton").click();
-		document.getElementById("lastGameChat").innerText = "Restore Chat";
+	    document.getElementById("lastGameChat").innerText = "Restore Chat";
 	} catch(err) {
-		document.getElementById("lastGameChat").innerText = "Too Fast";
+	    document.getElementById("lastGameChat").innerText = "Too Fast";
 	}
 }
 
@@ -41,7 +40,7 @@ function getRestoredChat() {
     document.getElementById("filterIP").value = filterIP;
     document.getElementById("filterUserId").value = filterUserId;
     document.getElementById("filterGameId").value = "";
-	document.getElementById("filterDisplayName").value = "";
+    document.getElementById("filterDisplayName").value = "";
     document.getElementById("filterButton").click();
     document.getElementById("lastGameChat").innerText = "Last Game Chat";
 }
